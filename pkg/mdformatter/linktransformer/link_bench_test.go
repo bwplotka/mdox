26c4495695f950a5b6cb81ae695cce56db331ffe
Saswata Mukherjee <saswataminsta@yahoo.com>
2021-07-12 22:35:08 +0530

    Added benchmarks (#56)
    * Add benchmarks
    
    Signed-off-by: Saswata Mukherjee <saswataminsta@yahoo.com>
    
    * Add link validator benchmark
    
    Signed-off-by: Saswata Mukherjee <saswataminsta@yahoo.com>

---
+++ pkg/mdformatter/linktransformer/link_bench_test.go
@@ -0,0 +1,61 @@
+// Copyright (c) Bartłomiej Płotka @bwplotka
+// Licensed under the Apache License 2.0.
+
+package linktransformer
+
+import (
+	"bytes"
+	"context"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/bwplotka/mdox/pkg/mdformatter"
+	"github.com/efficientgo/tools/core/pkg/testutil"
+	"github.com/go-kit/kit/log"
+)
+
+var testBuf bytes.Buffer
+
+var (
+	testDoc = `# Hey
+
+This is a test
+	
+# This is a section
+	
+This is a test`
+
+	testDocWithLink = `# Hello
+
+This is a test [link](./doc.md)
+
+This is a test section [link](./doc.md#this-is-a-section)`
+)
+
+func benchLinktransformer(b *testing.B) {
+	tmpDir, err := ioutil.TempDir("", "bench-test")
+	testutil.Ok(b, err)
+	b.Cleanup(func() { testutil.Ok(b, os.RemoveAll(tmpDir)) })
+
+	testutil.Ok(b, os.MkdirAll(filepath.Join(tmpDir, "repo", "docs"), os.ModePerm))
+	testutil.Ok(b, ioutil.WriteFile(filepath.Join(tmpDir, "repo", "docs", "doc.md"), []byte(testDoc), os.ModePerm))
+	testutil.Ok(b, ioutil.WriteFile(filepath.Join(tmpDir, "repo", "docs", "links.md"), []byte(testDocWithLink), os.ModePerm))
+	anchorDir := filepath.Join(tmpDir, "repo", "docs")
+	logger := log.NewLogfmtLogger(os.Stderr)
+
+	file, err := os.OpenFile(filepath.Join(tmpDir, "repo", "docs", "links.md"), os.O_RDONLY, 0)
+	testutil.Ok(b, err)
+	defer file.Close()
+
+	f := mdformatter.New(context.Background(), mdformatter.WithLinkTransformer(MustNewValidator(logger, []byte(""), anchorDir)))
+	b.ResetTimer()
+	b.Run("Validator", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			testutil.Ok(b, f.Format(file, &testBuf))
+		}
+	})
+}
+
+func Benchmark_Linktransformer(b *testing.B) { benchLinktransformer(b) }
---
+++ pkg/mdformatter/mdgen/mdgen_bench_test.go
@@ -0,0 +1,37 @@
+// Copyright (c) Bartłomiej Płotka @bwplotka
+// Licensed under the Apache License 2.0.
+
+package mdgen
+
+import (
+	"bytes"
+	"context"
+	"os"
+	"testing"
+
+	"github.com/bwplotka/mdox/pkg/mdformatter"
+	"github.com/efficientgo/tools/core/pkg/testutil"
+)
+
+var testBuf bytes.Buffer
+
+func benchMdgen(filename string, b *testing.B) {
+	file, err := os.OpenFile(filename, os.O_RDONLY, 0)
+	testutil.Ok(b, err)
+	defer file.Close()
+
+	f := mdformatter.New(context.Background(), mdformatter.WithCodeBlockTransformer(NewCodeBlockTransformer()))
+	b.ResetTimer()
+
+	b.Run(filename, func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			testutil.Ok(b, f.Format(file, &testBuf))
+		}
+	})
+}
+
+func Benchmark_Mdgen_Sleep2(b *testing.B) { benchMdgen("benchdata/sleep2.md", b) }
+
+func Benchmark_Mdgen_Sleep5(b *testing.B) { benchMdgen("benchdata/sleep5.md", b) }
+
+func Benchmark_Mdgen_GoHelp(b *testing.B) { benchMdgen("benchdata/gohelp.md", b) }
